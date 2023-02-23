b6ed4f2749060994858d198c7325c1065f17d3e0
Luka Agic <agic.luke@gmail.com>
2023-02-23 23:35:20 +0100

    Create App.php

---
+++ app/core/App.php
@@ -0,0 +1,31 @@
+<?php
+
+class App
+{
+
+    public static function start()
+    {
+        $route = Request::getRuta();
+        $parts = explode('/',substr($route,1));
+        $controller='';
+        if(!isset($parts[0]) || $parts[0]===''){
+            $controller='IndexController';
+        }else{
+            $controller = ucfirst($parts[0]) . 'Controller';
+        }
+        $method='';
+        if(!isset($parts[1]) || $parts[1]==='' ){
+            $method='index';
+        }else{
+            $method=$parts[1];
+        }
+        if(!(class_exists($controller) && method_exists($controller,$method))){
+            echo 'Ne postoji ' . $controller . '-&gt;' . $method;
+            return;
+        }
+        $instance = new $controller();
+        $instance->$method();
+    }
+
+
+}
