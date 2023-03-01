a209cc6ab04578f8a82b87a8e058471e9e75bb5a
Luka Agic <agic.luke@gmail.com>
2023-03-01 23:49:35 +0100

    Add login view page, validate password
    Validate if the password was entered

--- app/controller/LoginController.php
+++ app/controller/LoginController.php
@@ -5,7 +5,7 @@ class LoginController extends Controller
 {
     public function authorization()
     {
-        if(isset($_POST['email']) ||
+        if(!isset($_POST['email']) ||
         strlen(trim($_POST['email'])) === 0) {
             $this->view->render('login',[
                 'message' => 'You have to enter email',
@@ -13,5 +13,16 @@ class LoginController extends Controller
             ]);
             return;
         }
+
+        if(!isset($_POST['password']) ||
+        strlen(trim($_POST['password'])) === 0){
+            $this->view->render('login', [
+                'message' => 'Password required',
+                'email' => $_POST['email']
+            ]);
+            return;
+        }
     }
+
+
 }
