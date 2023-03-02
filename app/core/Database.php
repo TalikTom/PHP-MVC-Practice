95c5617a17b8c36b6d0f197bcbe707433f30ae0f
Luka Agic <agic.luke@gmail.com>
2023-03-02 01:39:50 +0100

    Create sql file

--- app/core/Database.php
+++ app/core/Database.php
@@ -2,23 +2,45 @@
 
 class Database extends PDO
 {
+
+    /*The purpose of the singleton pattern is to ensure that there is only ever one instance of the class in the application.
+    This is useful when working with databases,
+    as it ensures that multiple connections to the same database are not created unnecessarily, which can cause performance issues. */
+
+
+
+    /*The class has a private static variable called "$instance" that is initialized to null.
+     The purpose of this variable is to store a reference to the singleton instance of the class. */
     private static $instance = null;
 
     private function __construct()
     {
+
+        //extract variables from database key in config file using public static method config in App class
         extract(App::config('database'));
+
+        /*The "parent::__construct" line is  used to call the constructor of the parent PDO class,
+         passing in the database configuration data that was extracted earlier. */
         parent::__construct($dsn, $user, $password);
-        $this->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+
+        //result of database queries will be returned as objects.
+        $this->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
     }
 
+
+    /* The class also has a public static method called "getInstance" that is used to get a reference to the singleton instance of the class.
+    Inside this method, the "self::$instance" variable is checked to see if it is null.
+    If it is, a new instance of the class is created using the private constructor,
+    and the "$instance" variable is set to reference this new instance.
+    If "$instance" is not null, it means that an instance of the class has already been created, so the existing instance is returned. */
 
     public static function getInstance()
     {
-        if(self::$instanca==null){
-            self::$instanca=new self();
+        if (self::$instance == null) {
+            self::$instance = new self();
         }
 
-        return self::$instanca;
+        return self::$instance;
     }
-    
+
 }
---
+++ voyager.sql
@@ -0,0 +1,120 @@
+drop database if exists voyager;
+create database voyager charset utf8mb4;
+use voyager;
+
+create table department
+(
+    odjel_id  int         not null auto_increment primary key,
+    naziv     varchar(50) not null,
+    broj_soba int
+);
+
+create table patient
+(
+    pacijent_id int not null auto_increment primary key,
+    ime         varchar(50),
+    prezime     varchar(50),
+    adresa      varchar(255),
+    oib         char(11),
+    doktor_id   int
+);
+
+create table doctor
+(
+    doktor_id       int not null auto_increment primary key,
+    ime             varchar(50),
+    prezime         varchar(50),
+    specijalizacija varchar(50),
+    oib             char(11),
+    odjel_id        int
+);
+
+create table medical_record
+(
+    medicinski_karton_id int not null auto_increment primary key,
+    datum_pregleda       datetime,
+    dijagnoza            varchar(255),
+    pacijent_id          int
+);
+
+create table patient_visitor
+(
+    posjeta_id          int not null auto_increment primary key,
+    datum               datetime,
+    pacijent_id         int,
+    posjetitelj_id      int,
+    max_trajanje_minute int
+);
+
+create table visitor
+(
+    posjetitelj_id  int not null auto_increment primary key,
+    ime             varchar(50),
+    prezime         varchar(50),
+    adresa          varchar(50),
+    telefonski_broj int
+);
+
+alter table patient
+    add foreign key (doktor_id) references doctor (doktor_id);
+
+alter table patient_visitor
+    add foreign key (pacijent_id) references patient (pacijent_id);
+alter table patient_visitor
+    add foreign key (posjetitelj_id) references visitor (posjetitelj_id);
+
+alter table medical_record
+    add foreign key (pacijent_id) references patient (pacijent_id);
+
+alter table doctor
+    add foreign key (odjel_id) references department (odjel_id);
+
+insert into department (naziv, broj_soba)
+values ('Kardiologija', 15);
+insert into department (naziv, broj_soba)
+values ('Psihijatrija', 1);
+insert into department (naziv, broj_soba)
+values ('Neurologija', 5);
+
+
+
+insert into doctor (ime, prezime, specijalizacija, oib, odjel_id)
+values ('Imzo', 'Prezimic', 'Kardiologija', 45798462514, 1);
+insert into doctor (ime, prezime, specijalizacija, oib, odjel_id)
+values ('Hrvoje', 'Samic', 'Psihijatrija', 45798212514, 2);
+insert into doctor (ime, prezime, specijalizacija, oib, odjel_id)
+values ('Gojka', 'Prezimic', 'Neurologija', 45298462514, 3);
+
+insert into patient (ime, prezime, adresa, oib, doktor_id)
+values ('Marko', 'Marulic', 'Trosinska 21, Silas 31011', 15487569851, 1);
+insert into patient (ime, prezime, adresa, oib, doktor_id)
+values ('Jovica', 'Jovic', 'B.B. Billmayera 41, Osijek 31000', 15487569851, 3);
+insert into patient (ime, prezime, adresa, oib, doktor_id)
+values ('Sinan', 'Sakic', ' 21, Ernestinovo 31456', 15487239851, 2);
+
+insert into medical_record (datum_pregleda, dijagnoza, pacijent_id)
+values ('2022-09-09',
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley",
+        1);
+insert into medical_record (datum_pregleda, dijagnoza, pacijent_id)
+values ('2022-08-08',
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard",
+        2);
+insert into medical_record (datum_pregleda, dijagnoza, pacijent_id)
+values ('2022-12-12',
+        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+        3);
+
+insert into visitor (ime, prezime, adresa, telefonski_broj)
+values ('Posjet', 'Kasnic', 'Ul. Inatise 76, Zupanja 32260', '098784652');
+insert into visitor (ime, prezime, adresa, telefonski_broj)
+values ('Lakomisl', 'Trazic', 'Idarisa Sadrisa 2, Osijek 31000', '098782352');
+insert into visitor (ime, prezime, adresa, telefonski_broj)
+values ('Butro', 'Rasnic', 'Trg Slobode 1, Zupanja 32260', '098784642');
+
+insert into patient_visitor (datum, pacijent_id, posjetitelj_id, max_trajanje_minute)
+values ('2022-12-09', 1, 1, 15);
+insert into patient_visitor (datum, pacijent_id, posjetitelj_id, max_trajanje_minute)
+values ('2022-12-08', 2, 2, 15);
+insert into patient_visitor (datum, pacijent_id, posjetitelj_id, max_trajanje_minute)
+values ('2022-12-07', 3, 3, 15);
